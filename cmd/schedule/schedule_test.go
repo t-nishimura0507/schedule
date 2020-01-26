2092296980197189e65bde73bc87a26d8a9f3a76
T.Nishimu <nishimu0507@gmail.com>
2020-01-26 17:29:32 +0900

    Update schedule_test.go

--- cmd/schedule/schedule_test.go
+++ cmd/schedule/schedule_test.go
@@ -72,18 +72,3 @@ func TestGetValidationError(t *testing.T) {
 		t.Error("Response is Success...")
 	}
 }
-
-func TestGetUnsetEnvError(t *testing.T) {
-
-	// param
-	var param = "2020-01-22"
-
-	// exec
-	_, err := Get(param)
-
-	// assertion
-	if err == nil {
-		t.Error("Response is Success...")
-	}
-	t.Log(err.Error())
-}
